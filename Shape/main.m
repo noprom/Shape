6601df1b3eea0c0e66a308c1713f5a0a6e269625
noprom <tyee.noprom@qq.com>
2015-06-25 09:11:47 +0800

    Initial Commit

---
+++ Shape/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Shape
+//
+//  Created by noprom on 15/6/25.
+//  Copyright (c) 2015年 noprom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
